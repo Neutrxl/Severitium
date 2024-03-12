d5403f29853848895e25f98478522c0c5c6c84e6
Neutrxl <orakxmori@gmail.com>
2024-03-12 18:26:25 +0200

    Update

--- General/PlayerContextMenu/PlayerContextMenu.js
+++ General/PlayerContextMenu/PlayerContextMenu.js
@@ -1,20 +1,19 @@
 (function() {
 	document.addEventListener('DOMContentLoaded', function() {
-		const menu = document.querySelector('.ContextMenuStyle-menu');
-	
-		menu.addEventListener('click', function(event) {
-			event.stopPropagation();
-		});
-	
-		menu.addEventListener('animationend', function(event) {
-			if (event.animationName === 'fadeOutDown') {
-				menu.remove();
+		document.addEventListener('click', function(event) {
+			const menu = event.target.closest('.ContextMenuStyle-menu');
+			if (menu) {
+				event.stopPropagation();
+				if (!menu.contains(event.target)) {
+					menu.classList.add('fadeOutDown');
+				}
 			}
 		});
-	
-		document.addEventListener('click', function(event) {
-			if (!menu.contains(event.target)) {
-				menu.classList.add('fadeOutDown');
+
+		document.addEventListener('animationend', function(event) {
+			const menu = event.target.closest('.ContextMenuStyle-menu');
+			if (menu && event.animationName === 'fadeOutDown') {
+				menu.remove();
 			}
 		});
 	});
--- Theme.user.js
+++ Theme.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 
 // @name			Theme
-// @version			1.1.8
+// @version			1.1.9
 // @description		Custom theme for _game_
 // @author			OrakomoRi
 
