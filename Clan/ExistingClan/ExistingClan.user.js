d48a2feea846f916a0740efdc62628de5efcaa7f
Neutrxl <orakxmori@gmail.com>
2024-03-16 00:52:01 +0200

    Update

--- Clan/ExistingClan/ExistingClan.user.js
+++ Clan/ExistingClan/ExistingClan.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 
 // @name			CSS Existing Clan
-// @version			1.0.5
+// @version			1.0.6
 // @description		Injects CSS code into the page
 // @author			OrakomoRi
 
--- Theme.user.js
+++ Theme.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 
 // @name			Theme
-// @version			1.1.43
+// @version			1.1.44
 // @description		Custom theme for _game_
 // @author			OrakomoRi
 
