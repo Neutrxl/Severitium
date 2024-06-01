27b45de8671de26fa4f5272720c731ac5db5aab3
Neutrxl <OrakxmoRi@gmail.com>
2024-06-01 02:02:47 +0200

    Update 1.3.40+build.16

--- src/General/PlayerContextMenu/PlayerContextMenu.js
+++ src/General/PlayerContextMenu/PlayerContextMenu.js
@@ -29,15 +29,13 @@
 			// Set unique id for the span
             parentDiv.id = uniqueId;
 
-			console.log(`Div id: ${uniqueId}, span text color: ${spanColor}`);
-
 			// Check if the span color is red
 			if (spanColor === 'rgb(255, 124, 124)') {
 				// Create new style
 				var style = document.createElement('style');
 
 				// Style inner
-				style.innerHTML = `.ContextMenuStyle-menu > div#${uniqueId}:hover { background-color: rgba(225, 75, 75, 0.1) !important; } .ContextMenuStyle-menu > div#${uniqueId}:hover::before { background-color: rgba(225, 75, 75, 0.75) !important; }`;
+				style.innerHTML = `.ContextMenuStyle-menu > div#${uniqueId} > span { color: var(--severitium-red-text-color); } .ContextMenuStyle-menu > div#${uniqueId}:hover { background-color: rgba(225, 75, 75, 0.1) !important; } .ContextMenuStyle-menu > div#${uniqueId}:hover::before { background-color: rgba(225, 75, 75, 0.75) !important; }`;
 
 				// Append style to modal
 				modal.appendChild(style);
