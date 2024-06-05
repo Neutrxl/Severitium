40a6bd87538d70cbe71fc2789f3f5c634cf94d1d
Neutrxl <OrakxmoRi@gmail.com>
2024-06-05 03:34:40 +0200

    Update 1.4.0+build.6

--- release/Severitium.user.js
+++ release/Severitium.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 
 // @name			Severitium
-// @version			1.4.0+build.5
+// @version			1.4.0+build.6
 // @description		Custom theme for Tanki Online
 // @author			OrakomoRi
 
--- src/General/TopPanel/TopPanel.user.js
+++ src/General/TopPanel/TopPanel.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 
 // @name			CSS Top Panel
-// @version			1.0.4
+// @version			1.1.0
 // @description		Restyles the common look of the top panel
 // @author			OrakomoRi
 
